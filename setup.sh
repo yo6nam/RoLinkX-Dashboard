417835462ced861a83a49278346101dc4e2373d3
Razvan <32558308+yo6nam@users.noreply.github.com>
2021-08-23 17:27:06 +0300

    Basic installation script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,35 @@
 #!/bin/bash
 # RoLinkX Dashboard v0.1a
-# Required installation script code
+# Setup script for minimum dashboard requirements
 
-echo "ToDo..."
-exit 0
+if netstat -tnlp | grep "lighttpd" >/dev/null; then
+	printf 'Check : lighttpd is present\n'
+else
+	printf 'Check : lighttpd is not installed\n'
+	read -p "Do you want to proceed with installation? (y/n)" -n 1 -r
+	printf '\n'
+	if [[ $REPLY =~ ^[Yy]$ ]];then
+		sudo apt-get update
+		sudo apt-get install lighttpd php7.3-fpm php-cgi -y
+	fi
+fi
+
+if [ ! $(command -v php-cgi) ]; then
+	printf 'Check : php-cgi not installed, installing...\n'
+	sudo apt-get install php7.3-fpm php-cgi -y
+fi
+
+if [ -d "/var/www/html" ]; then
+    printf "Copying files...\n"
+    cp -r . /var/www/html/rolink
+    printf "Setting up permissions\n"
+    chown -R www-data /var/www/html/rolink
+    if [ -f /var/www/html/rolink/69_rolink ]; then
+    	printf "Giving access to certain commands\n"
+    	cp /var/www/html/rolink/69_rolink /etc/sudoers.d/69_rolink;
+    	rm -f /var/www/html/rolink/69_rolink
+    	rm -f /var/www/html/rolink/setup.sh
+    	[ -f /var/www/html/rolink/profiles/.gitignore ] && rm -f /var/www/html/rolink/profiles/.gitignore
+    fi
+fi
+printf "Done! You should access the dashboard using\nhttp://rolink/rolink or http://<IP>/rolink\n"
