6754fef9624f748f00b7323c252676f97e8eb3db
Razvan <32558308+yo6nam@users.noreply.github.com>
2021-12-09 14:57:32 +0200

    Update sys.php

--- ajax/sys.php
+++ ajax/sys.php
@@ -1,6 +1,6 @@
 <?php
 /*
-*   RoLinkX Dashboard v0.9k
+*   RoLinkX Dashboard v0.9l
 *   Copyright (C) 2021 by Razvan Marin YO6NAM / www.xpander.ro
 *
 *   This program is free software; you can redistribute it and/or modify
@@ -177,6 +177,6 @@ if ($makeRO == 1) echo makeRO();
 function makeRO() {
 	exec("/usr/bin/sudo /opt/rolink/scripts/init ro s", $reply);
 	sleep(3);
-	$result = ($reply[0] == 'Finished!') ? 'Operation succeeded!</br><b>Please reboot</b>' : '<b>Please reboot</b> to apply changes';
+	$result = ($reply[0] == '0') ? 'Operation succeeded!</br><b>Please reboot</b>' : 'Operation succeeded (no watchdog)</br><b>Please reboot</b>';
 	return $result;
 }
