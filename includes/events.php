67e6e15229006654067fdde599c300b2ddd3eaf6
Razvan <32558308+yo6nam@users.noreply.github.com>
2023-11-26 23:49:52 +0200

    Update events.php
    - added missing GPL preamble

--- includes/events.php
+++ includes/events.php
@@ -1,4 +1,26 @@
 <?php
+/*
+*   RoLinkX Dashboard v3.6
+*   Copyright (C) 2023 by Razvan Marin YO6NAM / www.xpander.ro
+*
+*   This program is free software; you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation; either version 2 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program; if not, write to the Free Software
+*   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+/*
+*   Real-time events
+*/
 ignore_user_abort(true);
 set_time_limit(0);
 header("Cache-Control: no-cache");
